663b5fb7d879a33d93813fcc49cd28d57a5b4928
Rishabh Kunwar <rishabh.kunwar04@gmail.com>
2025-05-22 21:34:35 +0530

    ...

--- JAVA/Java LLD/src/Creational_Pattern/singleton/Singleton.java
+++ JAVA/Java LLD/src/Creational_Pattern/singleton/Singleton.java
@@ -26,11 +26,13 @@ package Creational_Pattern.singleton;
 //}
 
 ////*** Eager Initialization *** //
+
 //// 1. eager initialization waste space it create obj in advance when class loaded,
 //// lets suppose client didnt need any obj to use then its just space wastage
+/// final prevents the instance from being reassigned after initialization.
 //
 //public class Singleton {
-//    private static Singleton instance=new Singleton();
+//    private static  final Singleton instance=new Singleton();
 //    private Singleton(){}
 //    public static Singleton getInstance(){
 //
@@ -39,7 +41,34 @@ package Creational_Pattern.singleton;
 //}
 
 
-//*** Thread safe lazy Initialization *** //
+///*** Thread safe lazy Initialization *** //
+
+///This is achieved by making the getInstance() method synchronized ensuring only one thread can execute this method at a time.
+
+///When a thread enters the synchronized method, it acquires a lock on the class object. Other threads must wait until the method is executed.
+/// this design has overhead when getinstance call frequenctly because all thread are getting blocked
+
+/*
+public class Singleton {
+    private static Singleton instance;
+    private Singleton(){}
+    public static synchronized Singleton getInstance(){
+
+                if (instance == null) {
+                    instance = new Singleton();
+                }
+        return instance;
+    }
+}
+
+*/
+
+/// *** Double-Checked Locking  ***//
+
+///This approach minimizes performance overhead from synchronization by only synchronizing when the object is first created.
+///It uses the volatile keyword to ensure that changes to the instance variable are immediately visible to other threads.
+
+
 
 public class Singleton {
     private static Singleton instance;
@@ -57,12 +86,14 @@ public class Singleton {
     }
 }
 
-////** ENUM Singleton
-////The Enum Singleton pattern is the most robust and concise way to implement a singleton in Java.
-//public enum Enumsingleton{
-//    INSTANCE;
-//
-//    public void doSomething(){
-//
-//    }
-//}
+////** ENUM Singleton ** //
+//The Enum Singleton pattern is the most robust and concise way to implement a singleton in Java.
+/*
+public enum Enumsingleton{
+    INSTANCE;
+
+    public void doSomething(){
+
+    }
+}
+ */
